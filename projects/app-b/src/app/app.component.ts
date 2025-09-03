04edfeff539e0287c30097545e36875fcc2b134a
Albert Williams W <albertwilliamsw@Alberts-MacBook-Pro.local>
2025-09-03 17:40:36 +0530

    Initial commit: monorepo with app-a, app-b, shared-ui

---
+++ projects/app-b/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { SharedBadgeComponent } from '../../../shared-ui/src/public-api';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, SharedBadgeComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'app-b';
+}
---
+++ projects/shared-ui/src/lib/shared-badge/shared-badge.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-shared-badge',
+  standalone: true,
+  imports: [],
+  templateUrl: './shared-badge.component.html',
+  styleUrl: './shared-badge.component.css'
+})
+export class SharedBadgeComponent {
+
+}
